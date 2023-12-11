03fc64cae4dc3d993662cc418225046e8251c183
Zapta <zapta@users.noreply.github.com>
2023-12-10 21:04:39 -0800

    Cleaned up comments.

--- tool/test.py
+++ tool/test.py
@@ -3,18 +3,18 @@ import time
 
 gp = GreenPakI2cDriver(port="COM14", control_code=0b0001)
 
-
+# Dump the eeprom before.
 data = gp.read_eeprom_bytes(0, 256)
 print(f"\nEEPROM before:")
 gp.hex_dump(data)
 print()
 
-
+# Fill the eeprom with a new value.
 gp.program_eeprom_pages(0, bytearray([0x43] * 256))
 # gp.program_eeprom_pages(0, bytearray(range(0, 256)))
 # gp.program_eeprom_pages(6, bytearray([0x00 , 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99,  0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]))
 
-
+# Dump the eeprom after.
 data = gp.read_eeprom_bytes(0, 256)
 print(f"\nEEPROM after:")
 gp.hex_dump(data)
