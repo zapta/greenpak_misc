da410033adf3d1f7e4e4255349484a4f112a41df
Zapta <zapta@users.noreply.github.com>
2023-12-19 10:50:01 -0800

    Work in progress.

--- tool/test.py
+++ tool/test.py
@@ -8,31 +8,36 @@ import time
 
 gp = GreenPakI2cDriver(port="COM14", control_code=0b0001)
 
+# Scan and print available greenpack devices.
 control_codes = gp.scan_devices()
-print(f"Control codes: {control_codes}")
+if control_codes:
+  print(f"Devices found control codes:")
+  for control_code in control_codes:
+      print(f"  {control_code:02d}")
+else:
+  print(f"No devices found")
 
-
-# Dump the eeprom before.
+# Dump device content before programming.
 data = gp.read_nvm_bytes(0, 256)
 print(f"\nNVM before:")
 hex_dump(data)
 print()
 
 
-# Get the new program
+# Load the new program from disk. 
 file_name = "test_data/blinky_nvm_fast.txt"
 # file_name = "test_data/blinky_nvm_slow.txt"
-
 print(f"Loading file {file_name}")
 data = read_bits_file(file_name)
-print(f"\nProgram file:")
+print(f"\nProgram loaded from file:")
 hex_dump(data)
 print()
 
 
+# Program the new program into the device.
 gp.program_nvm_pages(0, data)
 
-# Dump the eeprom after.
+# Dump device content after programming.
 data = gp.read_nvm_bytes(0, 256)
 print(f"\nNVM after:")
 hex_dump(data)
