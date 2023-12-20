f7749b551a05124335048729c1a53862f5cb5aed
Zapta <zapta@users.noreply.github.com>
2023-12-19 20:31:44 -0800

    Set up a skeleton with basic serial i/o.

--- pico_i2c/platformio/src/main.cpp
+++ pico_i2c/platformio/src/main.cpp
@@ -1,11 +1,27 @@
+#include <Adafruit_TinyUSB.h>
 #include <Arduino.h>
 #include <FreeRTOS.h>
 #include <task.h>
 
-int x = xTaskGetTickCount();
+#define LED
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // USB serial.
+  Serial.begin(115200);
+
+  // Hardware serial.
+  Serial1.setFIFOSize(1024);
+  Serial1.begin(115200);
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, true);
+  delay(300);
+  digitalWrite(LED_BUILTIN, false);
+  delay(300);
+  Serial.println("Loop");
+  Serial.flush();
+  Serial1.println("Hardware serial");
 }
